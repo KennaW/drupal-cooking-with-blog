442cfb711f286f88437a324b23fd552ace81e06c
kenna warsinske & tanner haffner <student@epicodus.com>
2015-06-03 16:47:11 -0700

    add dummy content and connected slideshow

---
+++ sites/all/themes/bootstrap_subtheme/template.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * template.php
+ */
+//function bootstrap_subtheme_preprocess_region(&$variables) {
+//  if (isset($variables['region']['sidebar_first'])) {
+//    $i = 0;
+//    foreach ($variables['classes_array'] as &$value) {
+//      if ($value == 'well') {
+//        unset($variables['classes_array'][$i]);
+//      }
+//      $i++;
+//    }
+//  }
+//}
